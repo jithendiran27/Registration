eb2bf0a51339fe82281a52061b71a2b30504cc78
jithendiran27 <jithendiran7@gmail.com>
2023-11-29 11:11:11 +0530

    steps number

--- src/app/tool-bar/tool-bar.component.ts
+++ src/app/tool-bar/tool-bar.component.ts
@@ -6,20 +6,5 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./tool-bar.component.css'],
 })
 export class ToolBarComponent {
-  @Input() currentStep = 1 + 0;
-  // currentStep!: number ;
-  steps!: string;
-
-  s = this.currentStep + 1;
-
-  ngOnInit() {
-    this.step();
-  }
-  step() {
-    if (this.currentStep <= 2) {
-      this.steps = `Step 1`;
-    } else {
-      this.steps = '';
-    }
-  }
+  @Input() currentStep = 0;
 }
